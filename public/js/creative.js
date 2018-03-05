e1a60b2653c5f76e6b8b06e4ed7d1dfdcffb6bdd
LogeswariGuru <logeswari.careator@gmail.com>
2018-03-05 17:44:58 +0530

    section padding increase

--- public/js/creative.js
+++ public/js/creative.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       // if (target.length) {
       //   $('html, body').animate({
-      //     scrollTop: (target.offset().top - 87)
+      //     scrollTop: (target.offset().top - 7)
       //   }, 1000, "easeInOutExpo");
       //   return false;
       // }
