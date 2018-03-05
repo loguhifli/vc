868f358e0e7faa1da08bf61fa8629b0a869dea1e
LogeswariGuru <logeswari.careator@gmail.com>
2018-03-05 17:39:50 +0530

    color at proper time

--- public/js/creative.js
+++ public/js/creative.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 27)
+          scrollTop: (target.offset().top - 17)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -23,7 +23,7 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 27
+    offset: 17
   });
 
   // Collapse Navbar
